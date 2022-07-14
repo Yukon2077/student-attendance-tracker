95d77afe1445c8ef3f83b420decdd19a0b9831ec
Yuvan <yuvankumar2077@gmail.com>
2022-07-14 22:30:35 +0530

    Second commit

--- app.py
+++ app.py
@@ -1,72 +1,129 @@
 from datetime import datetime
-from flask import *
+from flask import Flask, render_template, redirect, url_for, request, session
 from db import *
+import secrets
 
 app=Flask(__name__)
+app.secret_key = secrets.token_hex()
 
+#===================================ROUTING===================================#
+#-----------------------------------HOME------------------------------------#
 @app.route('/')
 def home():
-	return redirect(url_for('login'))
+	return render_template("index.html")
 
+#-----------------------------------ABOUT US-----------------------------------#
+@app.route('/about-us')
+def about_us():
+	return render_template("about-us.html")
+
+#-----------------------------------LOGIN-----------------------------------#
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		email = request.form['usr_email']
-		password = request.form['usr_passwd']
-		register_number = verifyStudent(email, password)
-		if register_number:
-			return redirect(url_for('student_dashboard', register_number = register_number), code = 307)
-		elif email == 'admin@admin.com' and password == 'admin':
+	if request.method == 'GET':
+		return render_template("login.html", failure = False)
+	else:
+		email = request.form['email']
+		password = request.form['password']
+		if 'staff' in request.form:
+			staff_id = verifyStaff(email, password)
+			if staff_id:
+				session['is_logged_in'] = True
+				session['staff_id'] = staff_id
+				return redirect(url_for('admin_dashboard'))
+			else:
+				return render_template("login.html", failure = True)
+		else:
+			student_id, staff_id = verifyStudent(email, password)
+			if student_id:
+				session['is_logged_in'] = True
+				session['staff_id'] = staff_id
+				session['student_id'] = student_id
+				return redirect(url_for('student_dashboard', student_id = student_id))
+			else:
+				return render_template("login.html", failure = True)
+		
+
+#-----------------------------------REGISTER-----------------------------------#
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+	if request.method == 'GET':
+		return render_template("register.html", failure = False)
+	else:
+		name = request.form['name']
+		email = request.form['email']
+		password = request.form['password']
+		staff_id = addStaff(name, email, password)
+		if staff_id:
+			session['is_logged_in'] = True
+			session['staff_id'] = staff_id
 			return redirect(url_for('admin_dashboard'))
 		else:
-			return render_template("login.html", info = '<p class="alert alert-danger mx-auto text-dark text-center">Invalid email or password</p>')
-	else:
-		return render_template("login.html")
+			return render_template("register.html", failure = True)
 	
+#-----------------------------------STAFF DASHBOARD-----------------------------------#
 @app.route('/admin')
 def admin_dashboard():
-	return render_template("admin_dashboard.html")	
+	if 'is_logged_in' in session and 'staff_id' in session:
+		staff_id = session['staff_id']
+		date_of_attendance = datetime.now().strftime('%Y-%m-%d')
+		return render_template("admin_dashboard.html", date_of_attendance = date_of_attendance)	
+	else:
+		return redirect(url_for('login'))
 
+#-----------------------------------ADD STUDENT-----------------------------------#
 @app.route('/add_student', methods = ['GET', 'POST'])
 def add_student():
-	if request.method == 'POST':
-		register_number = request.form['usr_regno']
-		name = request.form['usr_name']
-		email = request.form['usr_email']
-		password = request.form['usr_passwd']
-		result = addStudent(register_number, name, email, password)
-		if result:
-			return render_template("add_student.html", info = 'Student added sucessfully')
+	if 'is_logged_in' in session and 'staff_id' in session:
+		staff_id = session['staff_id']
+		if request.method == 'POST':
+			register_number = request.form['register_number']
+			name = request.form['name']
+			email = request.form['email']
+			password = request.form['password']
+			result = addStudent(register_number, name, email, password, staff_id)
+			if result:
+				return render_template("add_student.html", good = True)
+			else:
+				return render_template("add_student.html", bad = True)
 		else:
-			return render_template("add_student.html", info = 'Some error occured')
+			return render_template("add_student.html")
 	else:
-		return render_template("add_student.html")
+		return redirect(url_for('login'))
 
+#-----------------------------------ADD ATTENDANCE------------------------------------#
 @app.route('/add_attendance', methods = ['GET', 'POST'])
 def add_attendance():
-	date_of_attendance = datetime.now().strftime('%Y-%m-%d')
-	register_numbers = getAllRegisterNumbers()
-	if request.method == 'POST':
-		for i in register_numbers:
-			periods_attended = ''
-			for j in range(8):
-				check = str(i) + '_' + str(j)
-				isChecked = check in request.form
-				if isChecked:
-					periods_attended += '1'
-				else:
-					periods_attended += '0'
-			date_of_attendance = request.form['date_of_attendance']
-			addAttendance(i, date_of_attendance, periods_attended)
-		return render_template("admin_dashboard.html", info = 'Attendance added successfully')
+	if 'is_logged_in' in session and 'staff_id' in session:
+		staff_id = session['staff_id']
+		date_of_attendance = datetime.now().strftime('%Y-%m-%d')
+		students = getStudents(staff_id)
+		no_of_periods = int(request.args['no_of_periods'])
+		if request.method == 'POST':
+			for i in students:
+				periods_attended = ''
+				for j in range(no_of_periods):
+					check = str(i) + '_' + str(j)
+					isChecked = check in request.form
+					if isChecked:
+						periods_attended += '1'
+					else:
+						periods_attended += '0'
+				date_of_attendance = request.form['date_of_attendance']
+				addAttendance(staff_id, i[0], date_of_attendance, periods_attended)
+			return redirect(url_for('admin_dashboard'))
+		else:
+			return render_template("add_attendance.html", date_of_attendance = date_of_attendance,
+			students = students, no_of_periods = no_of_periods)
 	else:
-		return render_template("add_attendance.html", date_of_attendance = date_of_attendance,
-		register_numbers = register_numbers)
+			return redirect(url_for('login'))
 
-@app.route('/student/<int:register_number>', methods = ['POST', 'GET']) 
-def student_dashboard(register_number):
-	if request.method == 'POST':
-		student = getStudentDetails(register_number)
+#-----------------------------------STUDENT DASHBOARD------------------------------------#
+@app.route('/student/<int:student_id>') 
+def student_dashboard(student_id):
+	if 'is_logged_in' in session and 'student_id' in session:
+		student_id = session['student_id']
+		student = getStudentDetails(student_id)
 		name = student['main'][0][0]
 		email = student['main'][0][1]
 		total_periods = student['main'][0][2]
@@ -87,13 +144,38 @@ def student_dashboard(register_number):
 	else:
 		return redirect(url_for('home'))
 
-@app.route('/view_student_details')
+#-----------------------------------STUDENT DETAILS------------------------------------#
+@app.route('/view_attendance')
 def view_student_details():
-	date_of_attendance = request.args['date_of_attendance']
-	attendance = viewAttendance(date_of_attendance)
-	return render_template('view_student.html', 
-		date_of_attendance = date_of_attendance,
-		attendance = attendance)
+	if 'is_logged_in' in session and 'staff_id' in session:
+		staff_id = session['staff_id']
+		date_of_attendance = request.args['date_of_attendance']
+		attendance = viewAttendance(staff_id, date_of_attendance)
+		if len(attendance) > 0:
+			no_of_periods = len(attendance[0][3])
+		else:
+			no_of_periods = 0
+		return render_template('view_attendance.html', 
+			date_of_attendance = date_of_attendance,
+			attendance = attendance,
+			no_of_periods = no_of_periods)
+	else:
+			return redirect(url_for('login'))
 
+#-----------------------------------LOGOUT------------------------------------#
+@app.route('/logout')
+def logout():
+	session.pop('is_logged_in', None)
+	session.pop('staff_id', None)
+	session.pop('student_id', None)
+	return redirect(url_for('login'))
+
+#-----------------------------------STOPS CAHCE------------------------------------#
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    return response
+
+#===================================MAIN===================================#
 if(__name__ == '__main__'):
-	app.run(debug = True)
+	app.run(debug = True, port = 80, host="0.0.0.0")
--- db.py
+++ db.py
@@ -1,79 +1,124 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 
-DB_NAME = 'CSE-3.db'
+DB_NAME = 'database.db'
 db = sqlite3.connect(DB_NAME)
 
+#===================================CREATING TABLES===================================#
+#-----------------------------------STAFF------------------------------------#
 db.execute('''
-	CREATE TABLE IF NOT EXISTS Student(
-	register_number INTEGER PRIMARY KEY,
+	CREATE TABLE IF NOT EXISTS Staff(
+	staff_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name TEXT NOT NULL,
 	email TEXT UNIQUE NOT NULL,
-	password TEXT NOT NULL,
-	total_periods INTEGER DEFAULT 0 NOT NULL,
-	periods_attended INTEGER DEFAULT 0 NOT NULL);
+	password TEXT NOT NULL);
 	''')
 
+#-----------------------------------STUDENT------------------------------------#
+db.execute('''
+	CREATE TABLE IF NOT EXISTS Student(
+	student_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	staff_id INTEGER NOT NULL,
+	register_number TEXT NOT NULL,
+	name TEXT NOT NULL,
+	email TEXT NOT NULL,
+	password TEXT NOT NULL,
+	total_periods INTEGER DEFAULT 0 NOT NULL,
+	periods_attended INTEGER DEFAULT 0 NOT NULL,
+	FOREIGN KEY (staff_id) REFERENCES Staff (staff_id),
+	UNIQUE (staff_id, register_number));
+	''')
+
+#-----------------------------------STUDENT ATTENDANCE------------------------------------#
 db.execute('''
 	CREATE TABLE IF NOT EXISTS StudentAttendance(
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	register_number INTEGER NOT NULL,
+	attendance_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	staff_id INTEGER NOT NULL,
+	student_id INTEGER NOT NULL,
 	date_of_attendance TEXT NOT NULL,
 	periods_attended TEXT NOT NULL,
-	FOREIGN KEY (register_number) REFERENCES Student (register_number));
+	FOREIGN KEY (student_id) REFERENCES Student (student_id),
+	FOREIGN KEY (staff_id) REFERENCES Staff (staff_id));
 	''')
 
 db.commit()
 db.close()
 
-def addStudent(register_number, name, email, password):
-	try:
-		password = generate_password_hash(password)
-		db = sqlite3.connect(DB_NAME)
-		db.execute('''
-		INSERT INTO Student(register_number, name, email, password)
-		VALUES('%d', '%s', '%s', '%s');
-		''' %(int(register_number), name, email, password))
-
-		db.commit()
-		db.close()
-		return True
-	except:
-		return False
-
-def addAttendance(register_number, date_of_attendance, periods_attended):
+#===================================CREATE===================================#
+#-----------------------------------STAFF------------------------------------#
+def addStaff(name, email, password):
+	password = generate_password_hash(password)
 	db = sqlite3.connect(DB_NAME)
+
 	db.execute('''
-	INSERT INTO StudentAttendance(register_number, date_of_attendance, periods_attended)
-	VALUES('%d', '%s', '%s');
-	''' %(register_number, date_of_attendance, periods_attended))
+	INSERT INTO Staff(name, email, password)
+	VALUES('%s', '%s', '%s');
+	''' %(name, email, password))
+
+	db.commit()
+
+	cursor = db.execute('''
+	SELECT staff_id FROM Staff WHERE email = '%s' 
+	''' %email)
+	
+	staff_id = cursor.fetchall()
+	db.close()
+	return staff_id[0][0]
+
+#-----------------------------------STUDENT------------------------------------#
+def addStudent(register_number, name, email, password, staff_id):
+	password = generate_password_hash(password)
+	db = sqlite3.connect(DB_NAME)
+
+	db.execute('''
+	INSERT INTO Student(staff_id, register_number, name, email, password)
+	VALUES('%d', '%s', '%s', '%s', '%s');
+	''' %(int(staff_id), register_number, name, email, password))
+
+	db.commit()
+	db.close()
+	return True
+
+#-----------------------------------STUDENT ATTENDANCE------------------------------------#
+def addAttendance(staff_id, student_id, date_of_attendance, periods_attended):
+	db = sqlite3.connect(DB_NAME)
+
+	db.execute('''
+	INSERT INTO StudentAttendance(staff_id, student_id, date_of_attendance, periods_attended)
+	VALUES('%d', '%d', '%s', '%s');
+	''' %(staff_id, student_id, date_of_attendance, periods_attended))
 
 	db.execute('''
 	UPDATE Student
 	SET total_periods = total_periods + 8, periods_attended = periods_attended + %d
-	WHERE register_number = %d;
-	''' %(periodsAttended(periods_attended), register_number))
+	WHERE student_id = %d;
+	''' %(periodsAttended(periods_attended), student_id))
 	
 	db.commit()
 	db.close()
 
-def viewAttendance(date_of_attendance):
+#===================================READ===================================#
+#-----------------------------------STUDENT ATTENDANCE------------------------------------#
+def viewAttendance(staff_id, date_of_attendance):
 	db = sqlite3.connect(DB_NAME)
+
 	cursor = db.execute('''
-	SELECT register_number, date_of_attendance, periods_attended 
-	FROM StudentAttendance
-    WHERE date_of_attendance = '%s';
-	''' %date_of_attendance)
+	SELECT S.register_number, S.name, SA.date_of_attendance, SA.periods_attended 
+	FROM Student AS S INNER JOIN StudentAttendance AS SA
+	ON S.student_id = SA.student_id
+    WHERE SA.staff_id = '%d' AND SA.date_of_attendance = '%s';
+	''' %(staff_id, date_of_attendance))
 	
 	studentAttendance = cursor.fetchall()
 	db.close()
 	return studentAttendance
 	
-
+#-----------------------------------STUDENT------------------------------------#
 def verifyStudent(email, password):
 	db = sqlite3.connect(DB_NAME)
+
 	cursor = db.execute('''
-	SELECT register_number, password
+	SELECT student_id, password, staff_id
 	FROM Student
 	WHERE email = '%s';
 	''' %email)
@@ -82,11 +127,11 @@ def verifyStudent(email, password):
 	db.close()
 	if students:
 		if check_password_hash(students[0][1], password):
-			return students[0][0]
+			return (students[0][0], students[0][2])
 	else:
 		return False
 
-def getStudentDetails(register_number):
+def getStudentDetails(student_id):
 	
 	student = {}
 	db = sqlite3.connect(DB_NAME)
@@ -94,31 +139,64 @@ def getStudentDetails(register_number):
 	cursor = db.execute('''
 	SELECT name, email, total_periods, periods_attended
 	FROM Student
-	WHERE register_number = '%d';
-	''' %register_number)
+	WHERE student_id = '%d';
+	''' %student_id)
 
 	student['main'] = cursor.fetchall()
 
 	cursor = db.execute('''
 	SELECT date_of_attendance, periods_attended 
 	FROM StudentAttendance
-	WHERE register_number = '%d';
-	''' %register_number)
+	WHERE student_id = '%d';
+	''' %student_id)
 
 	student['content'] = cursor.fetchall()
 	db.close()
 	return student
 
-def getAllRegisterNumbers():
+#-----------------------------------STAFF------------------------------------#
+def verifyStaff(email, password):
 	db = sqlite3.connect(DB_NAME)
-	cursor = db.execute('''
-	SELECT register_number
-	FROM Student;
-	''')
 
-	allregisternumbers = [element for innerList in cursor.fetchall() for element in innerList]
+	cursor = db.execute('''
+	SELECT staff_id, email, password
+	FROM Staff
+	WHERE email = '%s';
+	''' %email)
+
+	staffs = cursor.fetchall()
 	db.close()
-	allregisternumbers.sort(key = int)
+	if staffs:
+		if check_password_hash(staffs[0][2], password):
+			return staffs[0][0]
+	else:
+		return False
+
+def getStaffDetails(staff_id):
+	db = sqlite3.connect(DB_NAME)
+
+	cursor = db.execute('''
+	SELECT name, email, no_of_students
+	FROM Staff
+	WHERE staff_id = '%d';
+	''' %staff_id)
+
+	staff = cursor.fetchall()
+	db.close()
+	return staff
+
+def getStudents(staff_id):
+	db = sqlite3.connect(DB_NAME)
+
+	cursor = db.execute('''
+	SELECT student_id, register_number, name
+	FROM Student
+	WHERE staff_id = '%d'
+	ORDER BY register_number;
+	''' %staff_id)
+
+	allregisternumbers = cursor.fetchall()
+	db.close()
 	return allregisternumbers
 
 def periodsAttended(periods_attended):
